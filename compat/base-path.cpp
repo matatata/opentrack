3f6f52e381ac49814dfb47512ed9362341a2f3f2
Matteo Ceruti <matteo.ceruti@gmail.com>
2023-08-24 00:13:39 +0200

    Fix app start crash on mac

--- compat/base-path.cpp
+++ compat/base-path.cpp
@@ -6,7 +6,9 @@
 
 const QString& application_base_path()
 {
+#ifndef __APPLE__
     assert(qApp && "logic error");
+#endif
     static QString path = QCoreApplication::applicationDirPath();
     return path;
 }
